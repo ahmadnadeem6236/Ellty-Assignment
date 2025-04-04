5231c350e5f1b77bfd2709c5638e1cdf2058444f
ahmadnadeem6236 <ahmadnadeem6236@gmail.com>
2025-04-05 01:13:32 +0530

    final touchup

--- app/component/RadioTick.tsx
+++ app/component/RadioTick.tsx
@@ -14,7 +14,7 @@ export default function RadioTick() {
         className={`w-[25px] h-[25px] border-[1px] rounded-[6px]  shadow flex justify-center items-center motion-reduce:transition-none hover:cursor-pointer ${
           isSelected
             ? "bg-[#2469F6]  border-[#2469F6] hover:bg-[#5087F8]"
-            : "bg-white border-[#CDCDCD] opacity-60 "
+            : "bg-white border-[#CDCDCD] hover:border-gray-500 opacity-60 "
         }`}
         onClick={handleClick}
       >

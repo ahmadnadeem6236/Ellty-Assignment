2948e1a90f7fec256e79f717bd45e28691b48fa8
ahmadnadeem6236 <ahmadnadeem6236@gmail.com>
2025-04-04 17:39:48 +0530

    button

---
+++ app/component/Button.tsx
@@ -0,0 +1,18 @@
+"use client";
+interface btnStyle {
+  text: string;
+}
+
+export default function Button({ text }: btnStyle) {
+  return (
+    <button
+      onClick={() => {
+        console.log("Button clicked");
+      }}
+    >
+      <div className="h-[40px] w-[340px] rounded-sm bg-[#FFCE22] flex justify-center items-center hover:bg-[#FFD84D] hover:cursor-pointer motion-reduce:transition-none">
+        <p className="font-normal text-sm text-[#1F2128]">{text}</p>
+      </div>
+    </button>
+  );
+}
